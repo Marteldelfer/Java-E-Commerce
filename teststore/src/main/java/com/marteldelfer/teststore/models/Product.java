1677fa6629c1f886dfe34d13338ac0e38b21782e
Murillo Ferraz <marteldelfer@gmail.com>
2024-08-05 22:09:15 -0300

    Creates Getters and Setters for Product
    I forgot to create getters and setters for the Product model. But here they are.

--- teststore/src/main/java/com/marteldelfer/teststore/models/Product.java
+++ teststore/src/main/java/com/marteldelfer/teststore/models/Product.java
@@ -23,4 +23,59 @@ public class Product {
     private String imageName;
     private Date createdAt;
 
+
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+    public String getBrand() {
+        return brand;
+    }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+    public double getPrice() {
+        return price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public int getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getImageName() {
+        return imageName;
+    }
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 }
