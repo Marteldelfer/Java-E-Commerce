c43e62c0a4b152ef5e44946b0448a562a61f4a3a
Murillo Ferraz <marteldelfer@gmail.com>
2024-08-09 01:41:03 -0300

    Fix Security Configuration
    Fixed the security configuration to allow the images to be shown while logedout

--- teststore/src/main/java/com/marteldelfer/teststore/config/SecurityConfig.java
+++ teststore/src/main/java/com/marteldelfer/teststore/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.marteldelfer.teststore.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -33,5 +34,13 @@ public class SecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring()
+        .antMatchers("/images/**");
+    }
+
+
     
 }
