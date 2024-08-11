0dfab03ca252ad3f7345e16dde602fe4ef6a7edb
Murillo Ferraz <marteldelfer@gmail.com>
2024-08-10 21:42:29 -0300

    Add remove-from-cart
    Add remove-from-cart functionality. Maybe i'll have a way to edit the quantity. Next step is probably to create the orders on database.

--- teststore/src/main/java/com/marteldelfer/teststore/controllers/CartController.java
+++ teststore/src/main/java/com/marteldelfer/teststore/controllers/CartController.java
@@ -99,4 +99,30 @@ public class CartController {
         model.addAttribute("listQuantity", listQuantity);
         return "cart.html";
     }
+
+    @GetMapping("/remove")
+    public String removeFromCart(
+        Model model,
+        @RequestParam int id
+    ) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String email = authentication.getName();
+        User user = userRepo.findByEmail(email);
+        int userId = user.getId();
+        Cart cart = cartRepo.findById(userId).get();
+        
+        List<Integer> products = cart.getIndexList();
+        List<Integer> listQuantity = cart.getQuantityList();
+
+        int index = products.indexOf(id);
+        products.remove(index);
+        listQuantity.remove(index);
+
+        cart.setIndexList(products);
+        cart.setQuantityList(listQuantity);
+
+        cartRepo.save(cart);
+
+        return "redirect:/cart";
+    }
 }
