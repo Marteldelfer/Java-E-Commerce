db7e0015351e3b8064dfa2e4982a65b3089aaed7
Murillo Ferraz <marteldelfer@gmail.com>
2024-09-18 21:32:37 -0300

    Creted docker image

--- teststore/src/test/java/com/marteldelfer/teststore/TeststoreApplicationTests.java
+++ teststore/src/test/java/com/marteldelfer/teststore/TeststoreApplicationTests.java
@@ -3,10 +3,10 @@ package com.marteldelfer.teststore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TeststoreApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
